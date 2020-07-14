37de4cc53670cbe6a5f91d8094bac1224482a753
lichang <lichang@koolearn.com>
2020-07-14 19:45:21 +0800

    添加消费类代码

--- springboot-learn/springboot-manage/src/main/java/com/lcyanxi/controller/UserController.java
+++ springboot-learn/springboot-manage/src/main/java/com/lcyanxi/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.lcyanxi.controller;
 
-
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.alibaba.fastjson.JSONObject;
 import com.google.common.collect.Lists;
@@ -23,6 +22,7 @@ import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.client.producer.SendStatus;
 import org.apache.rocketmq.common.message.Message;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,7 +36,7 @@ import java.util.List;
 @RestController
 public class UserController {
 
-    @Reference
+    @Resource
     private IUserLessonService userLessonService;
 
     @Resource
