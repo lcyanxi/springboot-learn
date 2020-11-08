6d1ca999d62afc8578d2cbfdb8c00c77686f4815
lcyanxi <845892601@qq.com>
2020-11-08 22:59:47 +0800

    添加测试代码

---
+++ springboot-learn/springboot-manage/src/main/java/com/lcyanxi/util/CanReliveObj.java
@@ -0,0 +1,43 @@
+package com.lcyanxi.util;
+
+/**
+ * @author lichang
+ * @date 2020/11/8
+ */
+public class CanReliveObj {
+    // 类变量，属于GC Roots的一部分
+    public static CanReliveObj canReliveObj;
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println("调用当前类重写的finalize()方法");
+        canReliveObj = this;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        canReliveObj = new CanReliveObj();
+        canReliveObj = null;
+        System.gc();
+        System.out.println("-----------------第一次gc操作------------");
+        // 因为Finalizer线程的优先级比较低，暂停2秒，以等待它
+        Thread.sleep(2000);
+        if (canReliveObj == null) {
+            System.out.println("obj is dead");
+        } else {
+            System.out.println("obj is still alive");
+        }
+
+        System.out.println("-----------------第二次gc操作------------");
+        canReliveObj = null;
+        System.gc();
+        // 下面代码和上面代码是一样的，但是 canReliveObj却自救失败了
+        Thread.sleep(2000);
+        if (canReliveObj == null) {
+            System.out.println("obj is dead");
+        } else {
+            System.out.println("obj is still alive");
+        }
+
+    }
+}
