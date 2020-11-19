54f1a5225d5420110fcacfb5f1d3300f592657a4
lcyanxi <845892601@qq.com>
2020-11-19 23:12:48 +0800

    找到重复数据

--- springboot-learn/springboot-manage/src/main/java/com/lcyanxi/algorithm/FindRepeatNum.java
+++ springboot-learn/springboot-manage/src/main/java/com/lcyanxi/algorithm/FindRepeatNum.java
@@ -1,5 +1,8 @@
 package com.lcyanxi.algorithm;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 找出数组中重复的数字
  * 描述：在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，
@@ -15,6 +18,10 @@ public class FindRepeatNum {
     public static void main(String[] args) {
         Integer[] arr = {4,1,0,2,5,3,3};
         System.out.println(findRepeatNum(arr));
+        System.out.println(findRepeatNum2(arr));
+        int[] arr1 = {4,1,0,2,5,3,3};
+        System.out.println(findRepeatNum3(arr1));
+
 
     }
     private static Integer findRepeatNum(Integer[] arr){
@@ -28,6 +35,32 @@ public class FindRepeatNum {
                 temp = arr[i];
                 arr[i] = arr[temp];
                 arr[temp] = temp;
+            }
+        }
+        return -1;
+    }
+
+    private static Integer findRepeatNum2(Integer arr []){
+        Set<Integer> set = new HashSet<>();
+        for (Integer e : arr){
+            if (!set.add(e)){
+                return e;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 将数组映射到一个新数组的位子
+     * @param arr 数组
+     * @return
+     */
+    private static Integer findRepeatNum3(int[] arr){
+        int[] newArr = new int[arr.length];
+        for (Integer e : arr){
+            newArr[e] ++ ;
+            if (newArr[e] > 1){
+                return e;
             }
         }
         return -1;
