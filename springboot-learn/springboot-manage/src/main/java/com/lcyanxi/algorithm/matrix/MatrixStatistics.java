4a97d2236b42ca01ca907dd84f6d84630cc8a9e3
lcyanxi <845892601@qq.com>
2020-12-06 22:27:12 +0800

    二维数组统计1的个数代码优化

--- springboot-learn/springboot-manage/src/main/java/com/lcyanxi/algorithm/matrix/MatrixStatistics.java
+++ springboot-learn/springboot-manage/src/main/java/com/lcyanxi/algorithm/matrix/MatrixStatistics.java
@@ -24,18 +24,19 @@ public class MatrixStatistics {
         int[][] matrix = {
                 {0 ,0 ,0 ,1 ,1 ,1},
                 {0 ,0 ,0 ,0 ,0 ,1},
-                {0 ,0 ,0 ,1 ,1 ,1},
-                {0 ,0 ,0 ,1 ,1 ,1},
+                {1 ,1 ,1 ,1 ,1 ,1},
+                {0 ,0 ,1 ,1 ,1 ,1},
                 {0 ,0 ,0 ,0 ,0 ,0},
-                {0 ,0 ,0 ,1 ,1 ,1}
+                {1 ,1 ,1 ,1 ,1 ,1}
         };
 
         System.out.println(matrixStatistics(matrix));
+        System.out.println(matrixStatistics2(matrix));
     }
     // 这中从右上往左下遍历的方式问题，但是如果每一行的数据有100万个元素怎么办？ 还是挨个遍历吗
     private static List<Integer> matrixStatistics(int[][] matrix){
         List<Integer> list = Lists.newArrayList();
-        int max = 0;
+        int max = matrix[0].length - 1;
         int j = matrix[0].length - 1;
         for (int i = 0 ; i < matrix.length; i++){
             if (matrix[i][j] == 0){
@@ -45,23 +46,51 @@ public class MatrixStatistics {
                 list.add(i);
                 continue;
             }
-            int temp = max;
-            if (i != 0){
-                j --;
+            while (j > 0 && matrix[i][j - 1] == 1){
+                j -- ;
             }
-            for (; j > 0; j--){
-                if (matrix[i][j] == 1){
-                    temp ++;
-                }else {
-                    break;
-                }
-            }
-            j ++;
-            if (temp != max) {
+            if (max > j){
                 list.clear();
+                max = j;
             }
             list.add(i);
         }
         return list;
     }
+    // 每一行找是否有1通过二分查找
+    private static List<Integer> matrixStatistics2(int[][] matrix){
+        List<Integer> list = Lists.newArrayList();
+        int j = matrix[0].length - 1;
+        for (int i = 0 ; i < matrix.length; i++){
+            if (matrix[i][j] == 0){
+                continue;
+            }
+            if (j == 0){
+                list.add(i);
+                continue;
+            }
+            int mid = matrixStatisticsUtil(matrix[i],0,j);
+            if (mid < j){
+                j = mid;
+                list.clear();
+            }
+            list.add(i);
+        }
+        return list;
+
+    }
+    private static int matrixStatisticsUtil(int[] arr,int left,int right){
+        int result = 0;
+        while (left <= right){
+            int mid = (left + right)/2;
+            if (arr[mid] == 1){
+                result = mid;
+                right = mid - 1;
+            }else {
+                left = mid + 1;
+            }
+        }
+        return result;
+    }
+
 }
