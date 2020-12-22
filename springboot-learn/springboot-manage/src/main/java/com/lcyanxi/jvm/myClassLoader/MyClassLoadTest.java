aaeeac940dad41a14484b7130bf66bda1efe96b9
lcyanxi <845892601@qq.com>
2020-12-22 20:18:02 +0800

    类加载机制

--- springboot-learn/springboot-manage/src/main/java/com/lcyanxi/jvm/myClassLoader/MyClassLoadTest.java
+++ springboot-learn/springboot-manage/src/main/java/com/lcyanxi/jvm/myClassLoader/MyClassLoadTest.java
@@ -1,5 +1,7 @@
 package com.lcyanxi.jvm.myClassLoader;
 
+import com.lcyanxi.service.ICountService;
+
 /**
  * @author lichang
  * @date 2020/12/22
@@ -7,5 +9,19 @@ package com.lcyanxi.jvm.myClassLoader;
 public class MyClassLoadTest {
     public static void main(String[] args) {
         new Thread(new MsgHandle()).start();
+    }
+}
+class MsgHandle implements Runnable {
+    @Override
+    public void run() {
+        while (true) {
+            ICountService manager = ManagerFactory.getManager(ManagerFactory.MY_MANAGER);
+            manager.count();
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
