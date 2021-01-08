b9d0b5aa969998b1fd8e27278b03ef1e31eb849c
lcyanxi <845892601@qq.com>
2021-01-08 23:20:06 +0800

    nio

--- springboot-learn/springboot-service/src/test/java/com/lcyanxi/springbootservice/Test.java
+++ springboot-learn/springboot-service/src/test/java/com/lcyanxi/springbootservice/Test.java
@@ -7,5 +7,11 @@ package com.lcyanxi.springbootservice;
 public class Test {
     public static void main(String[] args) {
         System.out.println(-1%2);
+        Integer a = null;
+        message(a);
+    }
+
+    private static void message(int a){
+        System.out.println(a);
     }
 }
