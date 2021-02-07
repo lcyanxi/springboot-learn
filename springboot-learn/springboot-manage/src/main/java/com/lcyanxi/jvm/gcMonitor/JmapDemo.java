b274cd032a9e6b70e5dd163cc62c2978e664c0a4
lcyanxi <845892601@qq.com>
2021-02-07 15:35:29 +0800

    JVM监控代码

---
+++ springboot-learn/springboot-manage/src/main/java/com/lcyanxi/jvm/gcMonitor/JmapDemo.java
@@ -0,0 +1,26 @@
+package com.lcyanxi.jvm.gcMonitor;
+
+import java.util.ArrayList;
+
+/**
+ * -Xms60m -Xmx60m -XX:SurvivorRatio=8
+ * @author lichang
+ * @date 2021/2/7
+ * 在发生OOM时自动导处dump文件 -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/Users/Desktop/a.hprof
+ */
+public class JmapDemo {
+
+    public static void main(String[] args) {
+        ArrayList<byte[]> list = new ArrayList<>();
+
+        for (int i = 0; i < 1000; i++) {
+            byte[] arr = new byte[1024 * 100];//100KB
+            list.add(arr);
+            try {
+                Thread.sleep(30);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
