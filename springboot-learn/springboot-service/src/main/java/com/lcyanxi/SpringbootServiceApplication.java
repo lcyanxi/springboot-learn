c7e2be50834d908a5b5e6982dad3ceb497441ab6
lcyanxi <845892601@qq.com>
2021-03-08 16:59:09 +0800

    自定义starter

--- springboot-learn/springboot-service/src/main/java/com/lcyanxi/SpringbootServiceApplication.java
+++ springboot-learn/springboot-service/src/main/java/com/lcyanxi/SpringbootServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-@EnableDubbo
 public class SpringbootServiceApplication {
 
     public static void main(String[] args) throws Exception{
