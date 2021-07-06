bc3683b09d396d8a777d096db3fbdbdf242ab604
lcyanxi <>
2021-07-06 14:24:36 +0800

    修改log4j2日志格式

--- springboot-learn/springboot-service/src/main/java/com/lcyanxi/SpringbootServiceApplication.java
+++ springboot-learn/springboot-service/src/main/java/com/lcyanxi/SpringbootServiceApplication.java
@@ -1,6 +1,5 @@
 package com.lcyanxi;
 
-import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
