b8173b0c29bacfec688b955b0770b27744900a3d
lichang <lichang@keep.com>
2021-09-02 20:46:27 +0800

    去掉无用文件

--- springboot-learn/springboot-api/src/main/java/com/lcyanxi/es/service/UserESRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.lcyanxi.es.service;
-//
-//import com.lcyanxi.es.model.UserES;
-//import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-//
-//public interface UserESRepository extends ElasticsearchRepository<UserES, Long> {
-//}
