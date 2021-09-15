9b9cec08c336777a1f5a711b6971b2f8df871ef3
lcyanxi <845892601@qq.com>
2021-09-15 20:27:50 +0800

    feature:去掉apollo相关配置

--- springboot-learn/springboot-service/src/main/java/com/lcyanxi/service/impl/UserServiceImpl.java
+++ springboot-learn/springboot-service/src/main/java/com/lcyanxi/service/impl/UserServiceImpl.java
@@ -3,8 +3,10 @@ package com.lcyanxi.service.impl;
 import com.lcyanxi.dto.UserMapper;
 import com.lcyanxi.model.User;
 import com.lcyanxi.service.IUserService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,12 +16,17 @@ import org.springframework.transaction.annotation.Transactional;
  * @date 2020/11/28
  */
 @DubboService
+@Slf4j
 public class UserServiceImpl implements IUserService {
     @Autowired
     private UserMapper userMapper;
 
+    @Value("${date.value}")
+    private String dataValue;
+
     @Override
     public User findUserByUserNamePassword(String userName, String password) {
+        log.info("findUserByUserNamePassword userName:{},password:{},dataValue:{}",userName,password,dataValue);
         return userMapper.findUserByUserNamePassword(userName,password);
     }
 
