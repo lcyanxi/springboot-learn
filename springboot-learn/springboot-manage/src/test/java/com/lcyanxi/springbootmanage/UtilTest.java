784cd350be6636276d2af6a2010b4f856604be60
lcyanxi <845892601@qq.com>
2021-11-25 14:34:28 +0800

    feature:添加日志

--- springboot-learn/springboot-manage/src/test/java/com/lcyanxi/springbootmanage/UtilTest.java
+++ springboot-learn/springboot-manage/src/test/java/com/lcyanxi/springbootmanage/UtilTest.java
@@ -1,24 +1,35 @@
 package com.lcyanxi.springbootmanage;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author : lichang
  * @desc : 描述信息
  * @since : 2021/11/16/10:15 上午
  */
+@Slf4j
 public class UtilTest {
-
+    private static final long LAST_CONNECTION_TIMEOUT = TimeUnit.MINUTES.toSeconds(10);
     @Test
-    public void test(){
+    public void test() {
         int index = 0;
-        while (index ++ < 100){
+        while (index++ < 100) {
             int score = 6;
+            long num = 19;
+            log.info("LAST_CONNECTION_TIMEOUT:{}",LAST_CONNECTION_TIMEOUT);
+            int rang = BigDecimal.valueOf(num).multiply(BigDecimal.valueOf(0.05)).intValue();
+            int newRange = BigDecimal.valueOf(num).multiply(BigDecimal.valueOf(0.05))
+                    .setScale(0, RoundingMode.HALF_UP).intValue();
+            log.info("num:{},rang:{},newRange:{}", (num - score),rang,newRange);
             Random random = new Random();
             Double newScore = random.doubles((score * 0.8), (score * 1.2)).findFirst().orElse(0);
-            System.out.println(newScore.intValue());
+            log.info("old:{},new:{}", newScore, Math.floor(newScore));
         }
     }
 }
