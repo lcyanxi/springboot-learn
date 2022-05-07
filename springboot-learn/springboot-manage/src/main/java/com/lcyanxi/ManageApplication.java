31e4eb2153ce61ee0bd646e63172e14cf2973e78
lcyanxi <845892601@qq.com>
2022-05-07 08:52:25 +0800

    格式化代码

--- springboot-learn/springboot-manage/src/main/java/com/lcyanxi/ManageApplication.java
+++ springboot-learn/springboot-manage/src/main/java/com/lcyanxi/ManageApplication.java
@@ -1,10 +1,6 @@
 package com.lcyanxi;
-
-
-
 import com.alicp.jetcache.autoconfigure.JetCacheAutoConfiguration;
 import com.lcyanxi.annotation.EnableCacheConfiguration;
-
 import com.lcyanxi.rocketmq.annotation.EnableMQConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
