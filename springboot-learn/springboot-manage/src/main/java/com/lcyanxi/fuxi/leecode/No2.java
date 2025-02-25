51649058cbd8ac8ed1e3bdd6a5253da8bf2b95a3
lcyanxi <845892601@qq.com>
2025-02-25 11:32:27 +0800

    leetcode 两数相加

---
+++ springboot-learn/springboot-manage/src/main/java/com/lcyanxi/fuxi/leecode/No2.java
@@ -0,0 +1,83 @@
+package com.lcyanxi.fuxi.leecode;
+
+/**
+ * 2. 两数相加
+ * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+ * <p>
+ * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+ * <p>
+ * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ * <p>
+ * 输入：l1 = [2,4,3], l2 = [5,6,4]
+ * 输出：[7,0,8]
+ * 解释：342 + 465 = 807.
+ * 示例 2：
+ * <p>
+ * 输入：l1 = [0], l2 = [0]
+ * 输出：[0]
+ * 示例 3：
+ * <p>
+ * 输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+ * 输出：[8,9,9,9,0,0,0,1]
+ */
+public class No2 {
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode cur = head;
+        int temp = 0;
+        while (l1 != null || l2 != null) {
+            int val1 = l1 == null ? 0 : l1.val;
+            int val2 = l2 == null ? 0 : l2.val;
+            temp += val2 + val1;
+            cur.next = new ListNode(temp % 10);
+            cur = cur.next;
+            temp = temp / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (temp == 1){
+            cur.next = new ListNode(temp);
+        }
+
+        return head.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        l1.next = new  ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new  ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        ListNode listNode = addTwoNumbers(l1, l2);
+        while (listNode != null){
+            System.out.print(listNode.val);
+            listNode = listNode.next;
+        }
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {}
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
+
+
+
