5a55b579f49910d5ecbd6975739c7dcf348526e8
lichang <845892601@qq.com>
2025-02-26 10:22:01 +0800

    leetcode No11

---
+++ springboot-learn/springboot-manage/src/main/java/com/lcyanxi/fuxi/leecode/No11.java
@@ -0,0 +1,40 @@
+package com.lcyanxi.fuxi.leecode;
+
+/**
+ * 11. 盛最多水的容器
+ *
+ * 给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
+ *
+ * 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+ *
+ * 返回容器可以储存的最大水量。
+ *
+ * 输入：[1,8,6,2,5,4,8,3,7]
+ * 输出：49
+ * 解释：图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。
+ * 示例 2：
+ *
+ * 输入：height = [1,1]
+ * 输出：1
+ */
+public class No11 {
+    public static  int maxArea(int[] height) {
+        int result = 0;
+        int left =0;
+        int right = height.length -1;
+        while (left < right){
+            result = Math.max(result,(right - left) * Math.min(height[left],height[right]));
+            if (height[left] < height[right]){
+                left ++;
+            }else {
+                right --;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int i = maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7});
+        System.out.println(i);
+    }
+}
