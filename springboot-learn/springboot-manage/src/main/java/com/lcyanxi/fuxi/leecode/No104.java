7c0f62e2e1dbeaf3661faee15e0931efa6d52845
lichang <845892601@qq.com>
2025-03-12 10:04:26 +0800

    leetcode No104

---
+++ springboot-learn/springboot-manage/src/main/java/com/lcyanxi/fuxi/leecode/No104.java
@@ -0,0 +1,53 @@
+package com.lcyanxi.fuxi.leecode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 104. 二叉树的最大深度
+ *
+ * 给定一个二叉树 root ，返回其最大深度。
+ *
+ * 二叉树的 最大深度 是指从根节点到最远叶子节点的最长路径上的节点数。
+ *
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：3
+ * 示例 2：
+ *
+ * 输入：root = [1,null,2]
+ * 输出：2
+ *
+ */
+public class No104 {
+    public static  int maxDepth(TreeNode root) {
+        if (root == null){
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList();
+        queue.add(root);
+        int depth = 0;
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            for (int i =0; i<size; i++){
+                TreeNode peek = queue.poll();
+                if (peek.left != null){
+                    queue.add(peek.left);
+                }
+                if (peek.right != null){
+                    queue.add(peek.right);
+                }
+            }
+            depth++;
+        }
+        return depth;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        System.out.println(maxDepth(root));
+    }
+}
