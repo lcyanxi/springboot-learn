8f198c325aaf8cec0706bf22cf463f8ee144cfa6
lichang <845892601@qq.com>
2025-03-13 19:56:47 +0800

    leetcode No215

---
+++ springboot-learn/springboot-manage/src/main/java/com/lcyanxi/fuxi/leecode/No107.java
@@ -0,0 +1,62 @@
+package com.lcyanxi.fuxi.leecode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 107. 二叉树的层序遍历 II
+ *
+ * 给你二叉树的根节点 root ，返回其节点值 自底向上的层序遍历 。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+ *
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：[[15,7],[9,20],[3]]
+ * 示例 2：
+ *
+ * 输入：root = [1]
+ * 输出：[[1]]
+ * 示例 3：
+ *
+ * 输入：root = []
+ * 输出：[]
+ */
+public class No107 {
+
+    public static List<List<Integer>> levelOrderBottom(TreeNode root) {
+        if (root == null ){
+            return new ArrayList<>();
+        }
+        List<List<Integer>> res = new ArrayList<>();
+        LinkedList<List<Integer>> list = new LinkedList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> temp = new ArrayList<>();
+            while (size >0){
+                TreeNode node = queue.poll();
+                temp.add(node.val);
+                size--;
+                if (node.left != null){
+                    queue.add(node.left);
+                }
+                if (node.right != null){
+                    queue.add(node.right);
+                }
+            }
+            list.addFirst(temp);
+        }
+         res.addAll(list);
+        return res;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        System.out.println(levelOrderBottom(root));
+    }
+}
