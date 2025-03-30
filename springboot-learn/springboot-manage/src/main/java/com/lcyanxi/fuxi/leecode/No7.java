ce929371c598fbe6fec9f006b7f8dcd36b29fc43
lichang <845892601@qq.com>
2025-03-30 16:12:03 +0800

    lettcode No7

---
+++ springboot-learn/springboot-manage/src/main/java/com/lcyanxi/fuxi/leecode/No7.java
@@ -0,0 +1,38 @@
+package com.lcyanxi.fuxi.leecode;
+
+/**
+ * 7. 整数反转
+ *示例 1：
+ *
+ * 输入：x = 123
+ * 输出：321
+ *
+ * 示例 3：
+ *
+ * 输入：x = 120
+ * 输出：21
+ * 思路： 取最后一位 x % 10 取剩下的 x / 10
+ *
+ *
+ */
+public class No7 {
+    public static int reverse(int x) {
+        int result = 0;
+        while (x !=0){
+            result = result * 10 + x % 10;
+            if (result > 214748364 || (result == 214748364 && x % 10 >7)){
+                return 0;
+            }
+            if (result < -214748364 || (result == -214748364 && x% 10 <-8)){
+                return 0;
+            }
+            x= x/10;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse(-12300));
+    }
+
+}
