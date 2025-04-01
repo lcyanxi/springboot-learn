d0e902186cf64a42a4a36976dc6b6a85bb754878
lichang <845892601@qq.com>
2025-04-01 21:11:37 +0800

    leetcode No234

--- springboot-learn/springboot-manage/src/main/java/com/lcyanxi/fuxi/leecode/linkedList/No234.java
+++ springboot-learn/springboot-manage/src/main/java/com/lcyanxi/fuxi/leecode/linkedList/No234.java
@@ -3,6 +3,15 @@ package com.lcyanxi.fuxi.leecode.linkedList;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 234. 回文链表
+ *
+ * 给你一个单链表的头节点 head ，请你判断该链表是否为回文链表。如果是，返回 true ；否则，返回 false 。
+ * 输入：head = [1,2,3,4,3,2,1]
+ * 输出：true
+ *
+ * 思路： 后半部分链表反转
+ */
 public class No234 {
     public static boolean isPalindrome(ListNode head) {
         if (head == null || head.next == null) {
@@ -33,22 +42,45 @@ public class No234 {
         }
         ListNode fast = head;
         ListNode slow = head;
-        while (fast.next != null && fast.next.next !=  null){
+        while (fast != null && fast.next != null){
             fast = fast.next.next;
             slow = slow.next;
         }
-
-        return false;
+        // 找到中间节点
+        if (fast == null){
+            slow  = slow.next;
+        }
+        // 反转链表
+        reverse(slow);
+        fast = head;
+        while (slow != null){
+            if (slow.val != fast.val){
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return true;
 
     }
 
+    private static void reverse(ListNode head){
+        ListNode pre = null;
+        ListNode curr = head;
+        while (curr != null){
+            ListNode next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+    }
 
     public static void main(String[] args) {
         ListNode head = new ListNode(1);
         head.next = new ListNode(2);
         head.next.next = new ListNode(2);
         head.next.next.next = new ListNode(1);
-        boolean palindrome = isPalindrome(head);
+        boolean palindrome = isPalindrome2(head);
         System.out.println(palindrome);
     }
 }
