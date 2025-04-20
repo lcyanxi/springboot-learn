14275a157d3378cc259f1a8316d82a87db9ea87a
lcyanxi <845892601@qq.com>
2025-04-21 00:12:08 +0800

    feature: 快排
    快排
    
    Signed-off-by: lcyanxi <845892601@qq.com>

--- springboot-learn/springboot-manage/src/main/java/com/lcyanxi/fuxi/algorithm/QuickSort.java
+++ springboot-learn/springboot-manage/src/main/java/com/lcyanxi/fuxi/algorithm/QuickSort.java
@@ -8,30 +8,44 @@ package com.lcyanxi.fuxi.algorithm;
  * eg: 输入：arr = [5,4,7,1,2,8,9]    输出：[1,2,4,5,7,8,9]
  */
 public class QuickSort {
+
+    private static void quickSort2(int[] num){
+        if (num == null || num.length == 0){
+            return;
+        }
+        quick(num,0,num.length-1);
+    }
+
+    private static void quick(int[] num, int left , int right){
+        if (left >= right){
+            return;
+        }
+        int mid = process(num, left, right);
+        quick(num,left,mid);
+        quick(num,mid +1 ,right);
+    }
+
+    private static int  process(int[] num,int left ,int right){
+        int target = num[left];
+        while (left < right){
+            while (left < right && num[left] < target){
+                left ++;
+            }
+            while (left < right && num[right] > target){
+                right--;
+            }
+            int temp = num[left];
+            num[left] = num[right];
+            num[right] = temp;
+        }
+        num[left] = target;
+        return left;
+    }
     static int [] arr = new int[]{5,4,7,1,2,8,9};
     public static void main(String[] args) {
-         quickSort(0,arr.length-1);
+        quickSort2(arr);
          for (int a : arr){
              System.out.print(a);
          }
-    }
-    public static void quickSort(int left, int right){
-        if (left < right){
-            int mid = process(left, right);
-            quickSort(left,mid);
-            quickSort(mid+1,right);
-        }
-    }
-    
-    private static int  process(int left, int right){
-        int temp = arr[left];
-        while (left < right){
-            while (left < right && temp <arr[right]) right --;
-            arr[left] = arr[right];
-            while (left < right && temp > arr[left]) left++;
-            arr[right] = arr[left];
-        }
-        arr[left] = temp;
-        return left;
     }
 }
