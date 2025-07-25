400b7774ee9716853fc5ebec21c599dfbee5275c
chang.li <chang.li@brgroup.com>
2025-07-25 19:42:48 +0800

    场景设计

--- springboot-learn/springboot-manage/src/main/java/com/lcyanxi/fuxi/leecode/matrix/No54.java
+++ springboot-learn/springboot-manage/src/main/java/com/lcyanxi/fuxi/leecode/matrix/No54.java
@@ -20,37 +20,36 @@ public class No54 {
     public static List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> result = new ArrayList<>();
         int left = 0;
-        int right = matrix[0].length - 1;
+        int right = matrix[0].length -1;
         int top = 0;
-        int bottom = matrix.length - 1;
-        while (true) {
-            for (int i = left; i <= right; i++) {
+        int bottom = matrix.length-1;
+        while (true){
+            for (int i = left; i <= right; i++){
                 result.add(matrix[top][i]);
             }
             top++;
             if (top > bottom){
                 break;
             }
-
-            for (int i = top; i <= bottom; i++) {
+            for (int i = top ; i<=bottom;i++){
                 result.add(matrix[i][right]);
             }
             right--;
             if (right < left){
                 break;
             }
-            for (int i = right; i >= left; i--) {
+            for (int i = right; i>= left; i--){
                 result.add(matrix[bottom][i]);
             }
             bottom--;
             if (bottom < top){
                 break;
             }
-            for (int i = bottom;i>= top;i--){
+            for (int i = bottom; i>= top;i--){
                 result.add(matrix[i][left]);
             }
             left++;
-            if ( left > right){
+            if (left > right){
                 break;
             }
         }
