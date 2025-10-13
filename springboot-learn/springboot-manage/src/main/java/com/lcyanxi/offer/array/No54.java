29d5a453407c81e7e405c61e4d7d1fbce6d06a74
chang.li <chang.li@brgroup.com>
2025-10-13 20:14:17 +0800

    leetcode No54

---
+++ springboot-learn/springboot-manage/src/main/java/com/lcyanxi/offer/array/No54.java
@@ -0,0 +1,49 @@
+package com.lcyanxi.offer.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给你一个 m 行 n 列的矩阵 matrix ，请按照 顺时针螺旋顺序 ，返回矩阵中的所有元素。
+ *
+ * 输入：matrix = [[1,2,3],
+ *                [4,5,6],
+ *                [7,8,9]]
+ * 输出：[1,2,3,6,9,8,7,4,5]
+ * @author chang.li
+ * @date 2025/10/13
+ * @version 1.0
+ */
+public class No54 {
+    public static void main(String[] args) {
+        int[][] matrix ={{1,2,3},{4,5,6},{7,8,9}};
+        System.out.println(process(matrix));
+    }
+
+    private static List<Integer> process(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
+        int left = 0;
+        int right = matrix[0].length - 1;
+        int top = 0;
+        int bottom = matrix.length - 1;
+        while (left <= right && top <= bottom) {
+            for (int i = left; i <= right; i++) {
+                res.add(matrix[top][i]);
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                res.add(matrix[i][right]);
+            }
+            right--;
+            for (int i = right; i >= left; i--) {
+                res.add(matrix[bottom][i]);
+            }
+            bottom--;
+            for (int i = bottom; i >= top; i--) {
+                res.add(matrix[i][left]);
+            }
+            left++;
+        }
+        return res;
+    }
+}
