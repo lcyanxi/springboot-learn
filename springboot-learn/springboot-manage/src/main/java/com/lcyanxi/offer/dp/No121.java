66aa5ed5db261c99dfdad58c0f86627686d97c01
chang.li <chang.li@brgroup.com>
2025-10-20 19:27:58 +0800

    leetcode No122

---
+++ springboot-learn/springboot-manage/src/main/java/com/lcyanxi/offer/dp/No121.java
@@ -0,0 +1,71 @@
+package com.lcyanxi.offer.dp;
+
+/**
+ * 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+ *
+ * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+ *
+ * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+ *
+ * 示例 1：
+ *
+ * 输入：[7,1,5,3,6,4]
+ *
+ * 输出：5
+ * 解释：在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
+ *
+ * 示例 2：
+ *
+ * 输入：prices = [7,6,4,3,1]
+ *
+ * 输出：0
+ * 解释：在这种情况下, 没有交易完成, 所以最大利润为 0。
+ * @author chang.li
+ * @date 2025/10/20
+ * @version 1.0
+ */
+public class No121 {
+    public static void main(String[] args) {
+        int[] arr = {7, 1, 5, 3, 6, 4};
+        System.out.println(process(arr));
+        System.out.println(process2(arr));
+
+    }
+
+    /**
+     * 记录历史最低点， 当前值 - 最底点记录最大利润
+     */
+    private static Integer process2(int[] arr) {
+        int max = 0;
+        int minPrice = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < minPrice) {
+                minPrice = arr[i];
+            } else if (arr[i] > minPrice) {
+                max = Math.max(max, arr[i] - minPrice);
+            }
+        }
+
+        return max;
+    }
+
+    /**
+     * 超时
+     */
+    private static Integer process(int[] arr) {
+        int result = 0;
+        int min = arr[0];
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > min) {
+                continue;
+            }
+            min = Math.min(min, arr[i]);
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] < arr[j]) {
+                    result = Math.max(result, arr[j] - arr[i]);
+                }
+            }
+        }
+        return result;
+    }
+}
--- springboot-learn/springboot-manage/src/main/java/com/lcyanxi/offer/dp/No718.java
+++ springboot-learn/springboot-manage/src/main/java/com/lcyanxi/offer/dp/No718.java
@@ -17,13 +17,67 @@ package com.lcyanxi.offer.dp;
  */
 public class No718 {
     public static void main(String[] args) {
-
+        int[] numsA = {1, 2, 3, 2, 1};
+        int[] numsB = {3, 2, 1, 4, 7};
+        System.out.println(process(numsA, numsB));
+        System.out.println(process2(numsA, numsB));
     }
 
+    /**
+     * 动态规划：
+     * i-1 = j-1 : dp[i][j] = dp[i-1]dp[j-1] + 1
+     *
+     */
+    private static Integer process2(int[] numsA, int[] numsB) {
+        int[][] dp = new int[numsA.length + 1][numsB.length + 1];
+        int res = 0;
+        for (int i = 1; i < numsA.length + 1; i++) {
+            for (int j = 1; j < numsB.length + 1; j++) {
+                if (numsA[i - 1] == numsB[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    res = Math.max(res, dp[i][j]);
+                }
+            }
+        }
+        return res;
+    }
+
+    /**
+     * 暴力解法： N^3
+     * 例子：A=[1,2,3,2,1], B=[3,2,1,4,7]
+     * 当i=0，j=0: A[0]=1, B[0]=3 -> 不匹配
+     * 当i=0，j=1: A[0]=1, B[1]=2 -> 不匹配
+     * 当i=0，j=2: A[0]=1, B[2]=1 -> 匹配，然后向后比较：A[1]=2, B[3]=4 -> 不匹配，所以长度为1。
+     * 当i=1，j=0: A[1]=2, B[0]=3 -> 不匹配
+     * 当i=1，j=1: A[1]=2, B[1]=2 -> 匹配，然后向后比较：A[2]=3, B[2]=1 -> 不匹配，所以长度为1。
+     * 当i=1，j=2: A[1]=2, B[2]=1 -> 不匹配
+     * 当i=2，j=0: A[2]=3, B[0]=3 -> 匹配，然后向后比较：A[3]=2, B[1]=2 -> 匹配；A[4]=1, B[2]=1 -> 匹配；然后A到末尾，B到B[2]之后是B[3]=4，所以长度为3。此时更新maxLen=3。
+     */
     private static Integer process(int[] numsA, int[] numsB) {
-        int left = 0;
-        int right = numsB.length - 1;
-        return 0;
+        int result = 0;
+        for (int i = 0; i < numsA.length; i++) {
+            for (int j = 0; j < numsB.length; j++) {
+                if (numsA[i] != numsB[j]) {
+                    continue;
+                }
+                result = Math.max(result, check(i, j, numsA, numsB));
+            }
+        }
+        return result;
+    }
+
+    private static Integer check(int i, int j, int[] numsA, int[] numsB) {
+        int temp = 0;
+        while (i < numsA.length && j < numsB.length) {
+            if (numsA[i] == numsB[j]) {
+                i++;
+                j++;
+                temp++;
+            } else {
+                break;
+            }
+        }
+        return temp;
     }
 
 }
